0ce3b41584d728fcefcafadc92a269188b74cde0
blobcode <lhelme123@gmail.com>
2022-01-11 18:04:52 -0500

    formatting

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-
-require('dotenv').config()
-const fs = require('fs');
+require("dotenv").config();
+const fs = require("fs");
 const Discord = require("discord.js");
 const settings = require("./settings.json");
 const { Client } = require("pg");
@@ -29,18 +28,22 @@ client.on("ready", () => {
   client.user.setActivity(settings.prefix + "help to see commands!");
 });
 
-client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.cache.find(channel => channel.name == settings.greetingChannel)
+client.on("guildMemberAdd", (member) => {
+  const channel = member.guild.channels.cache.find(
+    (channel) => channel.name == settings.greetingChannel
+  );
   if (!channel) return;
 
-  channel.send(`Welcome to the server, ${member.toString()}!`)
+  channel.send(`Welcome to the server, ${member.toString()}!`);
 });
 
-client.on('messageCreate', msg => {
-  if (msg.content.toLowerCase().substring(0, settings.prefix.length) == settings.prefix) {
-
+client.on("messageCreate", (msg) => {
+  if (
+    msg.content.toLowerCase().substring(0, settings.prefix.length) ==
+    settings.prefix
+  ) {
     content = msg.content.substring(2);
-    args = content.split(' ');
+    args = content.split(" ");
 
     //COMMANDS
     if (args[0] === "link") {
@@ -50,32 +53,33 @@ client.on('messageCreate', msg => {
       msg.reply("pong!");
     }
     if (args[0] === "set") {
-      if(check(msg)){
+      if (check(msg)) {
         set(msg, args);
-      } else{
+      } else {
         msg.reply("Sorry not an admin.");
       }
     }
-    if(args[0] == "check"){
-      if(check(msg)){
+    if (args[0] == "check") {
+      if (check(msg)) {
         msg.reply("You are an admin");
-      } else{
+      } else {
         msg.reply("Sorry not an admin.");
       }
     }
   }
 });
 
-function check(msg){ //checks if the author of a message is an admin. Returns a boolean.
+//checks if the author of a message is an admin. Returns a boolean.
+function check(msg) {
   return settings.admins.includes(msg.author.tag);
 }
 
 function set(msg, args) {
-  settings[args[1]] = args[2]
+  settings[args[1]] = args[2];
   fs.writeFile("./settings.json", JSON.stringify(settings), (error) => {
     if (error) return console.log(error);
-  })
-  msg.reply(`set \`${args[1]}\` to \`${args[2]}\``)
+  });
+  msg.reply(`set \`${args[1]}\` to \`${args[2]}\``);
 }
 
 // login with token from .env
